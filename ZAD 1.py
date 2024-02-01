cd44bb7df5d8e32333afcddb3429b006e1a02c3d
Patryk <144446197+tenxitm@users.noreply.github.com>
2024-02-01 17:52:43 +0100

    Add files via upload

---
+++ ZAD 1.py
@@ -0,0 +1,42 @@
+k = {
+    "tytul": "Jak pokochać informatykę ;)",
+    "liczba_stron": 1000,
+    "Autor": "Patryk"
+}
+
+def wyswietlenie_danych():          
+    for p, w in k.items():
+        print(f'{p} : {w}')
+        print("=" * 20)
+
+def edycja():
+    while True:
+        print("EDYCJA, co chcesz edytować:")
+        print(" t - tytuł")
+        print(" l - liczba stron")
+        print(" a - autor")
+        print(" e - exit")
+        inp = input("Wybierz opcję: ")
+
+        if inp == "t":
+            nowy_tytul = input("Nowy tytuł: ")
+            k["tytul"] = nowy_tytul
+        elif inp == "l":
+            nowa_liczba_stron = input("Nowa liczba stron: ")
+            k["liczba_stron"] = int(nowa_liczba_stron)
+        elif inp == "a":
+            nowy_autor = input("Nowy autor: ")
+            k["Autor"] = nowy_autor
+        elif inp == "e":
+            break
+        else:
+            print("Nie ma takiej komendy!")
+
+# Wywołanie funkcji do wyświetlenia danych
+wyswietlenie_danych()
+
+# Wywołanie funkcji do edycji danych
+edycja()
+
+# Wywołanie funkcji do ponownego wyświetlenia danych po edycji
+wyswietlenie_danych()
